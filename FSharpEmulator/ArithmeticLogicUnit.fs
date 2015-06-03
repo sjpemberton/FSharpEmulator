a1893201eca192a340a4b6baa3e26924897acde7
sjpemberton <sjpemberton09@gmail.com>
2015-06-03 16:35:33 +0100

    Add explicit PM module

--- FSharpEmulator/ArithmeticLogicUnit.fs
+++ FSharpEmulator/ArithmeticLogicUnit.fs
@@ -1,59 +1,64 @@
 ﻿module ArithmeticLogicUnit
 
+module PatternMatched =
+
+    let Nand a b = 
+        match a, b with
+        | true, true -> false
+        | _, _ -> true
+
+    let Not a = function
+        | true -> false
+        | false -> true
+
+    let And a b = 
+        match a, b with
+        | true, true -> true
+        | _, _ -> false
+
+    let Or a b =
+        match a, b with
+        | true, false -> true
+        | false, true -> true
+        | true, true -> true
+        | false, false -> false 
+
+    let Xor a b =
+        match a, b with
+        | true, false -> true
+        | false, true -> true
+        | _, _ -> false
+
+    let Mux a b sel =
+        match sel with
+        | false -> a
+        | _ -> b
+
+    let DMux x sel =
+        match sel with
+        | false -> (x,false)
+        | _ -> (false,x)
+
 let Nand a b = 
     match a, b with
     | true, true -> false
     | _, _ -> true
 
-
-let NotPM a = function
-    | true -> false
-    | false -> true
-
 let Not a = Nand a a
 
-let AndPM a b = 
-    match a, b with
-    | true, true -> true
-    | _, _ -> false
-
 let And a b = Nand a b |> Not
-
-let OrPM a b =
-    match a, b with
-    | true, false -> true
-    | false, true -> true
-    | true, true -> true
-    | false, false -> false 
 
 let Or a b =
     Nand (Nand a a) (Nand b b)
 
-let XorPM a b =
-    match a, b with
-    | true, false -> true
-    | false, true -> true
-    | _, _ -> false
-
 let Xor a b = 
     Or (And a (Not b)) (And (Not a) b) 
-
-let MuxPM a b sel =
-    match sel with
-    | false -> a
-    | _ -> b
 
 let Mux sel a b =
     Nand (Nand a sel) (Nand (Not sel) b)    
 
-let DMuxPM x sel =
-    match sel with
-    | false -> (x,false)
-    | _ -> (false,x)
-
 let DMux x sel =
     (And x (Not x), And x sel)
-
 
 let unaryArray gate bits =
     bits |> Array.map gate
@@ -61,10 +66,6 @@ let unaryArray gate bits =
 let binaryArray gate aBits bBits =
     Array.zip aBits bBits 
     |> unaryArray gate
-
-//let tertiaryArray func a b c =
-//    Array.zip a b 
-//    |> unaryArray func
 
 let MultiNot = unaryArray Not
 
@@ -92,7 +93,7 @@ let Mux8Way16 a b c d e f g h (sel:bool array) =
 let DMux4Way x (sel:bool array) = 
     let (d1,d2) = DMux x sel.[1]
     let (a,b) = DMux d1 sel.[0]
-    let (c,d) =  DMux d2 sel.[0]
+    let (c,d) = DMux d2 sel.[0]
     (a,b,c,d)
 
 let DMux8Way x (sel:bool array) = 
