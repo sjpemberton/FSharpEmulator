1acc766abc4da06db755bb5b90567b4836ef769b
Steve Pemberton <sjpemberton09@gmail.com>
2015-06-05 21:30:24 +0100

    Finish initial ALU

--- FSharpEmulator/ArithmeticLogicUnit.fs
+++ FSharpEmulator/ArithmeticLogicUnit.fs
@@ -54,6 +54,10 @@ let Or a b =
 let Xor a b = 
     Or (And a (Not b)) (And (Not a) b) 
 
+let XNOR a b =
+    Nand (Or a b) (Nand a b) 
+
+//Basic selector
 let Mux sel a b =
     Nand (Nand a sel) (Nand (Not sel) b)    
 
@@ -79,6 +83,8 @@ let MultiDMux sel = unaryArray (DMux sel)
     
 let MultiWayOr bits = 
     bits |> Array.reduce Or
+
+let MultiXNOR = binaryArray (fun (a,b) -> XNOR a b)
 
 let Mux4Way16 a b c d (sel:bool array) = 
     let m1 = MultiMux sel.[0] a b 
@@ -112,15 +118,51 @@ let FullAdder a b c =
     let (sum,c2) = HalfAdder s1 c
     (sum, Or c1 c2)
 
+//Ripple Carry Adder Implementation
 let Adder aBits bBits =
     let rec addBits aBits bBits carry accu = 
         match aBits, bBits with
         | aHead :: aTail, bHead :: bTail -> 
             let (sum,c) = FullAdder aHead bHead carry
             addBits aTail bTail c (sum :: accu)
-        | [_],_
-        | _,[_]
-        | _,_ -> accu
-    addBits aBits bBits false List.empty
-    |> List.rev
+        | [],_
+        | _,[]
+        | [],[] -> accu
+    addBits (aBits |> List.rev) (bBits |> List.rev) false List.empty
     |> List.toArray
+
+//In plus one
+let Incrementer aBits = Adder (aBits |> List.ofArray) [ for i in 1 .. 16 -> match i with | 16 -> true | _ -> false ]
+
+let ALU xBits yBits nx zx ny zy f no = 
+    //handle x    
+    let ox1 = MultiMux zx xBits [|for i in 1..16 -> false |]  //Zero all X bits if zx
+    let nox1 = MultiNot ox1 //What would this be if negated
+    let ox2 = MultiMux nx ox1 nox1 //Select based on nx
+
+    //handle y
+    let oy1 = MultiMux zy yBits [|for i in 1..16 -> false |]  //Zero all X bits if zy
+    let noy1 = MultiNot oy1 //What would this be if negated
+    let oy2 = MultiMux ny oy1 noy1 //Select based on ny
+
+    //handle & / +
+    let o3 = MultiAnd ox2 oy2 //an and would be
+    let o4 = Adder (List.ofArray ox2) (List.ofArray oy2) //addition would be
+
+    //Output
+    let o5 = MultiMux f o3 o4 //Choose and or addition
+    let no5 = MultiNot o5 //Negated out would be
+    let out = MultiMux no o5 no5 //Choose to negate or not
+
+    let zr = Not (MultiWayOr out)
+    let ng = MultiWayOr (MultiAnd out [|for i in 1..16 -> match i with | 16 -> true | _ -> false|] )
+    
+    (out, zr, ng)
+
+
+//  // see if out is 0
+//    Comparator16(a=check, b[0..15]=false, out=zr);
+//
+//    // check out MSB to see if out < 0
+//    Comparator(a=MSB, b=true, out=ng);
+// 
---
+++ FSharpEmulator/Utils.fs
@@ -0,0 +1,9 @@
+﻿module Utils
+
+let toBools (ints:int array) = 
+    let intToBool = function
+        | 1 -> true
+        | _ -> false
+    ints
+    |> Array.map intToBool
+
