0ba541c0e555d85baa08c1d58ecc17aa97885baf
Steve Pemberton <sjpemberton09@gmail.com>
2015-07-23 23:42:42 +0100

    Added SR Latch implementations

--- FSharpEmulator/Sequential.fs
+++ FSharpEmulator/Sequential.fs
@@ -1,5 +1,7 @@
 ﻿module Sequential
 
+open ArithmeticLogicUnit
+
 //The DFF (Data Flip Flop)
 //I have skipped building this chip from combinatorial chips as it is long winded.
 
@@ -17,3 +19,33 @@ type DFF() =
         | _ -> ()
         pState
     
+
+//The Set-Reset (SR) Latch - The inputs can be thought of as negated (they need to be set to false to take affect!)
+//The 2 output vars (The latch state) are Q and !Q - It is assumed !Q should always be the inverse of Q
+//Setting both S and R to false will cause issues with this logic and Q and !Q will be equal!
+//As we cannot have a cyclic chip we need to model it with implicit state - This is not technically correct bug gets the job done!
+//Effectively the state represents the continuous current
+//This implementation has inherit propagation delay for an entire cycle but misses the subtlety of the tick-tock 
+type SRLatch() =
+    let mutable state = (false,false)
+    member x.execute s r = 
+        state <- (Nand s (snd state),
+                  Nand (fst state) r)
+        state
+
+
+//We could add the clk into the execute, but this is not needed at this stage.
+//I've included it for completeness/correctness. This adds that extra level of propagation delay.
+//
+type SRLatchClocked() =
+    let mutable state = (false,false)
+    member x.execute s r clk =
+        let pState = state
+        match clk with //Only set the state on a tock 
+        | true -> state <- 
+                  (Nand s (snd state),
+                   Nand (fst state) r)
+        | _ -> ()
+        pState 
+
+     
